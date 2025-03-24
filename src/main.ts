0d87171ba603c2134c547d0869320c0e921f669b
ebraimsambo <ebraimsambo@gmail.com>
2025-03-24 18:30:24 +0100

    rabbit ajust

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,14 @@ import { MessageModule } from './message.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessageModule);
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(MessageModule,{
+      transport: Transport.RMQ,
     options: {
       urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
       queue: 'messanger',
       queueOptions: { durable: true  },
     },
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen();
 }
 bootstrap();
