a2b4fa200ee95a10c11ee80ee45cc9db79031386
ebraimsambo <ebraimsambo@gmail.com>
2025-03-26 00:46:53 +0100

    add ports

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import { MessageModule } from './message.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(MessageModule,{
-      transport: Transport.RMQ,
+  const app = await NestFactory.create(MessageModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
     options: {
       urls: [process.env.RABBITMQ_URL || 'amqp://localhost:5672'],
       queue: 'messanger',
       queueOptions: { durable: true  },
     },
   });
-  await app.listen();
+  await app.startAllMicroservices();
+  await app.listen(process.env.port ?? 4002);
 }
 bootstrap();
